3c5cf3db9bab003b61978ccd4acc4f2cccfaf28b
mhkeller52 <mkeller52@bloomberg.net>
2018-01-03 14:53:59 -0500

    Better rollup config for error handling
    Signed-off-by: mhkeller52 <mkeller52@bloomberg.net>

--- build/tasks/rollup/helpers/printErrorFrame.js
+++ build/tasks/rollup/helpers/printErrorFrame.js
@@ -4,19 +4,22 @@ var chalk = require('chalk');
 var getCodeFrame = require('./getCodeFrame.js');
 
 module.exports = function printErrorFrame ({ code, loc, id, message }, cb) {
-  fs.readFile(id, 'utf-8', function (err, source) {
-    if (err) {
-      notify({
-        message: 'Error reading file',
-        value: id,
-        error: err
-      });
-    } else {
-      let frame = getCodeFrame(source, loc.line, loc.columns).split('\n').map(d => '\t' + d).join('\n');
-      console.error('\n' + chalk.gray(frame));
-    }
-    if (cb) {
-      cb();
-    }
-  });
+  // Not all error objects will come from a snippet of code so check if there's a loc first
+  if (loc && loc.line) {
+    fs.readFile(id, 'utf-8', function (err, source) {
+      if (err) {
+        notify({
+          message: 'Error reading file',
+          value: id,
+          error: err
+        });
+      } else {
+        let frame = getCodeFrame(source, loc.line, loc.columns).split('\n').map(d => '\t' + d).join('\n');
+        console.error('\n' + chalk.gray(frame));
+      }
+      if (cb) {
+        cb();
+      }
+    });
+  }
 };
